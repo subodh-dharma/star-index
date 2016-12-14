f1abfb5429b71bb9be4d386046dbe7bdf744828a
ssdharma <ssdharma@ncsu.edu>
2016-12-14 09:55:09 +0530

    modify to export as function

--- github.js
+++ github.js
@@ -4,10 +4,20 @@ var gitclient = git.client();
 
 var ghuser = gitclient.user('subodh-dharma');
 
-ghuser.repos(function(error, repos, headers) {
-    for (repo in repos) {
-        //console.log(JSON.stringify(repos[repo], null, 4));
-        var r = repos[repo];
-        console.log("Repository: " + r.name, "\tStarred by " + r.stargazers_count + " developer(s)");
-    }
-});
+
+function getRepoStarCount(ghuser) {
+    return new Promise(function(resolve, reject) {
+        ghuser.repos(function(error, repos, headers) {
+            for (repo in repos) {
+                //console.log(JSON.stringify(repos[repo], null, 4));
+                var r = repos[repo];
+                console.log("Repository: " + r.name, "\tStarred by " + r.stargazers_count + " developer(s)");
+            }
+            resolve(repos);
+        });
+    });
+
+}
+
+
+exports.getRepoStarCount = getRepoStarCount;
